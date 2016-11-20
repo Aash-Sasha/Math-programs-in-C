e35fe6d20811c53aef537cd183fa21e6e55b4a46
Aash-Sasha <physicswithaash@gmail.com>
2016-11-20 19:40:31 +0530

    Update allinone.c

--- allinone.c
+++ allinone.c
@@ -6,6 +6,7 @@ int valuen();
 int factorialofn(int n);
 int primeornot(int n);
 int evenorodd(int n);
+int perfectnum(int n);
 void finishz();
 void main()
 {
@@ -27,18 +28,24 @@ void main()
 		finishz();
 		break;
 	case 3:
-		printf("Odd or Even");
+		printf("-=Odd or Even=-");
 		n = valuen();
 		evenorodd(n);
 		finishz();
 		break;
 	case 4:
+		printf("-=Sum of 1st n perfect number=-");
+		n = valuen();
+		perfectnum(n);
+		finishz();
+		break;
+	case 5:
 		exit();
 		break;
-	default :
+	default:
 		printf("Wrong Input,Try Again");
 		welcome();
-    }
+	}
 
 	_getch();
 }
@@ -46,7 +53,7 @@ int welcome()
 {
 	printf("MADE BY -=AaSh=-\n-=CALCULATOR=-\n");
 	printf("Input\n");
-	printf("1 To find Factorial of n\n2 To check n is prime or not\n3 To check n is odd or even\n4 To exit\n");
+	printf("1 To find Factorial of n\n2 To check n is prime or not\n3 To check n is odd or even\n4 To find Sum of 1st n perfect number\n5 To exit\n");
 
 	int z;
 	scanf_s("%d", &z);
@@ -56,23 +63,23 @@ int valuen()
 {
 	int k;
 	printf("\nEnter the value of n\n");
-	scanf_s("%d",&k);
+	scanf_s("%d", &k);
 
 	return (k);
 }
 int factorialofn(int n)
 {
-	int a,b;
+	int a, b;
 	for (a = 1, b = 1; a <= n; a = a + 1)
 	{
 		b = b*a;
 	}
-	printf("\nFactorial %d is %d",n,b);
+	printf("\nFactorial %d is %d", n, b);
 	return 0;
 }
 int primeornot(int n)
 {
-	int a,b,k,j;
+	int a, b, k, j;
 	k = 0;
 	j = n % 2;
 	if (j == 0)
@@ -87,14 +94,14 @@ int primeornot(int n)
 			b = n%a;
 			if (b == 0)
 			{
-				printf("\n%d is not a prime number",n);
+				printf("\n%d is not a prime number", n);
 				k = k + 1;
 				break;
 			}
 		}
 	}
 	if (k == 0)
-		printf("\n%d is a prime number",n);
+		printf("\n%d is a prime number", n);
 	return 0;
 }
 int evenorodd(int n)
@@ -105,11 +112,43 @@ int evenorodd(int n)
 		printf("%d is Odd", n);
 	return 0;
 }
+int perfectnum(int n)
+{
+	int a, b, asum, u_sum, k;
+	k = 0;
+	asum = 0;
+	u_sum = 0;
+	printf("\n Numbers are\n\n");
+	for (a = 5;; a = a + 1)
+	{
+		for (b = 1; b < a; b = b + 1)
+		{
+			if (a%b == 0)
+			{
+				asum = asum + b;
+			}
+
+		}
+		if (asum == a)
+		{
+			printf("%d\n", a);
+			u_sum = u_sum + asum;
+			k = k + 1;
+			asum = 0;
+		}
+		else
+			asum = 0;
+		if (k == n)
+			break;
+	}
+	printf("\nSum of 1st %d perfect number is %d\n", n, u_sum);
+	return 0;
+}
 void finishz()
 {
 	int p;
 	printf("\nINPUT 1 to restart\nINPUT 2 to exit\n");
-	scanf_s("%d",&p);
+	scanf_s("%d", &p);
 
 	if (p == 1)
 		main();
