3f4fd5f546d5204c0e11b673dc499d24ebddc639
Aash-Sasha <physicswithaash@gmail.com>
2016-11-20 19:51:55 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -2,15 +2,15 @@
 #include <conio.h>
 
 void main()
-{
-  
+{  
   int a, b,n;
   printf("ENTER A NUMBER\n");
   scanf_s("%d",&n);
- 	for (a = 1, b = 1; a <= n; a = a + 1)
-	{
-		b = b*a;
-	}
-	printf("\nFactorial %d is %d", n, b);
-  
+  for (a = 1, b = 1; a <= n; a = a + 1)
+  {
+	  b = b*a;
+  }
+	
+  printf("\nFactorial %d is %d", n, b);
+  _getch(); 	
 }  
