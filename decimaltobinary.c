9e7f9f9013e88bb3b3e685ab3fca66dbf03810e9
Aash-Sasha <physicswithaash@gmail.com>
2016-12-01 18:06:02 +0530

    Update decimaltobinary.c

--- decimaltobinary.c
+++ decimaltobinary.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 #include <conio.h>
-#include <math.h>
+
 //AaSh
+long long int personalpow(long long int x,long long int y);
 void main()
+
 {
-	long int r, a, j, q;
-	printf("Input Value of n\n");
-	scanf_s("%ld", &q);
-	
+	long long int r, a, j, q;
+	printf("\t-=AaSh=-\nInput Value of n\n\n");
+	scanf_s("%lld", &q);
+	printf("\n-==Decimal to Binary==-\nVersion 2(final version)\n");
 	j = 0;
 	for (a = 0; q > 1;a=a+1) 
 	{
 		r = q % 2;
+		if (q > 1)
+			printf("2|%lld|%lld\n------\n", q, r); 
 		q = q / 2;	
-		j = (r*pow(10, a)) + j;
+		j = (r*personalpow(10, a)) + j;
+		if (q <= 1)
+			printf(" |%lld|", q);
 	}
-	j = (1 * pow(10, a) + j);
-	printf("\n%ld",j);
+	j = (q * personalpow(10, a) + j);
+	printf("\n\n%lli\n",j);
 	_getch();
+}
+
+long long int personalpow (long long int x,long long int y)
+{
+	long long int k,c=1,j=x;
+	if (y == 0)
+		j = 1;
+	else
+		for (k = 1; k <= y; k = k + 1)
+		{
+			c =c*j;
+		}
+	return (c);
 }
 
