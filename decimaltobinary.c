1c244fedc88eaabf62be808665f38e0d29e447e5
Aash-Sasha <physicswithaash@gmail.com>
2016-12-02 16:46:44 +0530

    Update decimaltobinary.c

--- decimaltobinary.c
+++ decimaltobinary.c
@@ -9,7 +9,7 @@ void main()
 	int storage[90];
 	long long int r, a, j,q,k;
 
-	for (k = 1; k <= 19; k = k + 1)
+	for (k = 1; k <= 90; k = k + 1)
 	{
 		storage[k] = 0;
 	}
@@ -30,7 +30,7 @@ void main()
 	}
 	storage[a + 1] = q;
 	printf("\n\nFinal Answer\n\n");
-	for (j = 19; j >= 1; j = j - 1)
+	for (j = 90; j >= 1; j = j - 1)
 	{
 		printf("%d", storage[j]);
 	}
