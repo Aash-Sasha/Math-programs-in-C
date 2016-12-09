6501022e5c9094c6a1ccca127a58ac15795e3272
Aash-Sasha <physicswithaash@gmail.com>
2016-12-09 13:32:57 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+
+/*
+long long int personalpow(long long int x, long long int y)
+{
+	long long int k, c = 1, j = x;
+	if (y == 0)
+		j = x;
+	else
+		for (k = 1; k <= y; k = k + 1)
+		{
+			c = c*j;
+		}
+	return (c);
+}
+*/
