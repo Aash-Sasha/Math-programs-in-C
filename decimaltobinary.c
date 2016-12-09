12f1bb2693fc085c6e938c644d32c6b0d76fff8a
Aash-Sasha <physicswithaash@gmail.com>
2016-12-09 14:12:12 +0530

    Update decimaltobinary.c

--- decimaltobinary.c
+++ decimaltobinary.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
-#include <conio.h>
 
 //AaSh
+//version 4
+//checked on gcc-linux
+
 void main()
-
 {
-	int storage[64];
-	unsigned long long int r, a, j,q,k;
+	int storage[64],j,k;
+	unsigned long long int r,a,q;
 
+        //make all element of storage array 0 
 	for (k = 0; k <= 64; k = k + 1)
 	{
 		storage[k] = 0;
 	}
+
+        //print welcome message and take input
 	printf("\t-=AaSh=-\nInput Value of n\n\n");
-	scanf_s("%llu", &q);
+	scanf("%llu", &q);
+        q=ULLONG_MAX;
 	printf("\n-==Decimal to Binary==-\nVersion 4\n\n");
-	//j = 0;
+
+        //calculation and print table
 	for (a = 0; q > 1; a = a + 1)
 	{
 		r = q % 2;
@@ -27,10 +33,11 @@ void main()
 		storage[a] = r;
 	}
 	storage[a] = q;
-	printf("\n\nFinal Answer\n\n");
+
+        //print storage array in reverse
+	printf("\n\nFinal Answer\n\n");//
 	for (j = 64; j >= 0; j = j - 1)
 	{
 		printf("%d", storage[j]);
 	}
-	_getch();
 }
